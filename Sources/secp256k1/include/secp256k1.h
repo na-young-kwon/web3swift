38ff8f03336ddf3c138bc52b1aa843f51a57ad52
권나영 <kny0651@snplab.io>
2024-10-18 12:06:02 +0900

    수정

--- Sources/secp256k1/include/secp256k1.h
+++ Sources/secp256k1/include/secp256k1.h
@@ -759,6 +759,13 @@ SECP256K1_API SECP256K1_WARN_UNUSED_RESULT int secp256k1_ec_pubkey_combine(
                                                                   const unsigned char *privkey
                                                                   ) SECP256K1_ARG_NONNULL(1) SECP256K1_ARG_NONNULL(2) SECP256K1_ARG_NONNULL(3) SECP256K1_ARG_NONNULL(4);
 
+SECP256K1_API SECP256K1_WARN_UNUSED_RESULT int secp256k1_ecdh_impl(
+                                                              const secp256k1_context* ctx,
+                                                              unsigned char *result,
+                                                              const secp256k1_pubkey *pubkey,
+                                                              const unsigned char *privkey
+                                                              ) SECP256K1_ARG_NONNULL(1) SECP256K1_ARG_NONNULL(2) SECP256K1_ARG_NONNULL(3) SECP256K1_ARG_NONNULL(4);
+
 
 #ifdef __cplusplus
 }
